80e48d27fbd717709dc81899b2e3d72d18e936ec
SainyTK <tanakorn0412@gmail.com>
2023-12-09 20:38:39 +0700

    Initial commit

---
+++ tests/contract.test.ts
@@ -0,0 +1,68 @@
+import {
+  assert,
+  describe,
+  test,
+  clearStore,
+  beforeAll,
+  afterAll
+} from "matchstick-as/assembly/index"
+import { Address } from "@graphprotocol/graph-ts"
+import { ExampleEntity } from "../generated/schema"
+import { InsuranceCreated } from "../generated/Contract/Contract"
+import { handleInsuranceCreated } from "../src/contract"
+import { createInsuranceCreatedEvent } from "./contract-utils"
+
+// Tests structure (matchstick-as >=0.5.0)
+// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+
+describe("Describe entity assertions", () => {
+  beforeAll(() => {
+    let creator = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
+    let target = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
+    let asset = Address.fromString("0x0000000000000000000000000000000000000001")
+    let newInsuranceCreatedEvent = createInsuranceCreatedEvent(
+      creator,
+      target,
+      asset
+    )
+    handleInsuranceCreated(newInsuranceCreatedEvent)
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+
+  // For more test scenarios, see:
+  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+
+  test("ExampleEntity created and stored", () => {
+    assert.entityCount("ExampleEntity", 1)
+
+    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "creator",
+      "0x0000000000000000000000000000000000000001"
+    )
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "target",
+      "0x0000000000000000000000000000000000000001"
+    )
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "asset",
+      "0x0000000000000000000000000000000000000001"
+    )
+
+    // More assert options:
+    // https://thegraph.com/docs/en/developer/matchstick/#asserts
+  })
+})
