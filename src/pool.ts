b76b0172fc517770fafad807b4e5df691752360d
SainyTK <tanakorn0412@gmail.com>
2023-12-10 03:01:31 +0700

    Basic functionalities work

--- src/pool.ts
+++ src/pool.ts
@@ -1,40 +1,50 @@
 import { Address } from "@graphprotocol/graph-ts";
-import { AzurancePool } from "../generated/AzuranceFactory/AzurancePool";
+import { AzurancePool, InsuranceBought, InsuranceSold, StateChanged, Withdrew } from "../generated/AzuranceFactory/AzurancePool";
 import { InsurancePool } from "../generated/schema";
-import { getOrCreateToken } from "./token";
 
-export function getOrCreatePool(address: Address): InsurancePool {
+export function handleInsuranceBought(event: InsuranceBought): void {
+    updatePoolValue(event.address);
+}
+
+export function handleInsuranceSold(event: InsuranceSold): void {
+    updatePoolValue(event.address);
+}
+
+export function handleStateChanged(event: StateChanged): void {
+    let pool = updatePoolValue(event.address);
+    if (pool) {
+        pool.status = event.params.newState;
+        pool.save();
+    }
+}
+
+export function handleWithdrew(event: Withdrew): void {
+    updatePoolValue(event.address);
+}
+
+function updatePoolValue(address: Address): InsurancePool | null {
     let id = address.toHex();
     let pool = InsurancePool.load(id);
 
-    if (pool == null) {
-        pool = new InsurancePool(id);
+    let instance = AzurancePool.bind(address);
 
-        let instance = AzurancePool.bind(address);
+    let buyerShares = instance.try_totalBuyShare();
+    let sellerShares = instance.try_totalSellShare();
+    let totalShares = instance.try_totalSellShare();
 
-        let multiplier = instance.try_multiplier();
-        let multiplierDecimals = instance.try_multiplierDecimals();
-        let maturityBlock = instance.try_maturityBlock();
-        let staleBlock = instance.try_staleBlock();
-        let underlyingToken = instance.try_underlyingToken();
-        let fee = instance.try_fee();
-        let feeTo = instance.try_feeTo();
-        let condition = instance.try_condition();
-        let buyerToken = instance.try_buyerToken();
-        let sellerToken = instance.try_sellerToken();
-        let status = instance.try_status();
+    let totalValue = instance.try_totalValueLocked();
 
-        if (!multiplier.reverted) pool.multiplier = multiplier.value;
-        if (!multiplierDecimals.reverted) pool.multiplierDecimals = multiplierDecimals.value;
-        if (!maturityBlock.reverted) pool.maturityBlock = maturityBlock.value;
-        if (!staleBlock.reverted) pool.staleBlock = staleBlock.value;
-        if (!underlyingToken.reverted) pool.underlyingToken = getOrCreateToken(underlyingToken.value).id;
-        if (!fee.reverted) pool.fee = fee.value;
-        if (!feeTo.reverted) pool.feeTo = feeTo.value;
-        if (!condition.reverted) pool.condition = condition.value;
-        if (!buyerToken.reverted) pool.buyerToken = getOrCreatePool(buyerToken.value).id;
-        if (!sellerToken.reverted) pool.sellerToken = getOrCreatePool(sellerToken.value).id;
-        if (!status.reverted) pool.status = status.value;
+    if (pool && !buyerShares.reverted && !sellerShares.reverted && !totalShares.reverted && !totalValue) {
+        let buyerValue = buyerShares.value.times(totalValue.value).div(totalShares.value);
+        let sellerValue = sellerShares.value.times(totalValue.value).div(totalShares.value);
+
+        pool.buyerShares = buyerShares.value;
+        pool.sellerShares = sellerShares.value;
+        pool.totalShares = totalShares.value;
+
+        pool.buyerValue = buyerValue;
+        pool.sellerValue = sellerValue;
+        pool.totalValue = totalValue.value;
 
         pool.save();
     }
